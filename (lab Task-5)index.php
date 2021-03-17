fcb21bcf7d74378fa97f207267eb6376b72b83eb
purplecse111 <77968398+purplecse111@users.noreply.github.com>
2021-03-17 21:47:03 +0600

    Add files via upload

---
+++ (lab Task-5)index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+<body>
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"> Food Express</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        
+        
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+ <div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/cc1.jpg" alt="Burger" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="images/cc2.jpg" alt="Pizza" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="images/cc3.jpg" alt="Idli & Dosa" width="1100" height="500">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">
+			About Us
+		</h2>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+		<div class="col-lg-6 col-md-6 col-12">
+			<img src="images/cc4.jpg" class="img-fluid aboutimg">
+		</div>
+		<div class="col-lg-6 col-md-6 col-12">
+			<h2 class="display-4"> We are Food Express</h2>
+			<p class="py-3">
+				Foodexpress is an online food and delivery platform owned by Karnataka-based company Hero SB, which operates in about 30 countries across four continents. Hero is headquartered in Bangaluru, India.
+			</p>
+			<a href="about.php" class="btn btn-success" 
+			> Check More</a>
+		</div>
+	</div>
+	</div>
+</section>
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">
+			Our Services
+		</h2>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card" >
+                    <img class="card-img-top" src="images/cc5.jpg" alt="Card image">
+                  <div class="card-body">
+                      <h4 class="card-title">Delicious Food </h4>
+                      <p class="card-text">Some example text.</p>
+                      <a href="#" class="btn btn-primary">See Profile</a>
+                  </div>
+                </div>
+				
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card" >
+                    <img class="card-img-top" src="images/cc6.jpg" alt="Card image">
+                  <div class="card-body">
+                      <h4 class="card-title"> Coastal Food </h4>
+                      <p class="card-text">Some example text.</p>
+                      <a href="#" class="btn btn-primary">See Profile</a>
+                  </div>
+                </div>
+				
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card" >
+                    <img class="card-img-top" src="images/cc7.jpg" alt="Card image">
+                  <div class="card-body">
+                      <h4 class="card-title"> Platter Food </h4>
+                      <p class="card-text">Some example text.</p>
+                      <a href="#" class="btn btn-primary">See Profile</a>
+                  </div>
+                </div>
+				
+			</div>
+			
+		</div>
+		
+	</div>
+	</section>
+	<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Food Gallery </h2>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/cc8.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/cc9.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/cc10.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/cc11.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/cc12.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/cc13.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/cc14.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/cc15.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/cc16.jpg" class="img-fluid pb-4">
+			</div>
+			
+		</div>
+	</div>
+</section>
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center"> Contact Us </h2>
+	</div>
+	<div class="w-50 m-auto">
+		<form action="user.php" method="post">
+			<div class="form-group">
+				<label>Username</label>
+				<input type="text" name="user" autocomplete="off" class="form-control">
+				
+			</div>
+			<div class="form-group">
+				<label>Email</label>
+				<input type="text" name="email" autocomplete="off" class="form-control">
+				
+			</div>
+			<div class="form-group">
+				<label>Mobile</label>
+				<input type="text" name="mobile" autocomplete="off" class="form-control">
+				
+			</div>
+			<div class="form-group">
+				<label>Address</label>
+				<input type="text" name="address" autocomplete="off" class="form-control">
+				
+			</div>
+			<div class="form-group">
+				<label>Comment</label>
+				<textarea class="form-control">
+					
+				</textarea>
+				
+			</div>
+			<button type="submit" class="btn btn-success">
+				Submit
+				
+			</button>
+		</form>
+	</div>
+
+</section>
+<footer>
+	<h3 class="p-3 bg-dark text-white text-center">
+		@foodexpressDelivery
+	</h3>
+</footer>
+
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
