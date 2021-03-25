d0d0cd7ccd8a1170ccb47ff8b7dcf0c744f2edae
purplecse111 <77968398+purplecse111@users.noreply.github.com>
2021-03-26 04:06:54 +0600

    Add files via upload

---
+++ login home page(index,lab-6).php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login System</title>
+</head>
+<body>
+<h1>Login Home Page</h1>
+<table>
+<tr>
+<td>
+<input type="text" name="user_id" placeholder="User Id"/>
+</td>
+</tr>
+
+<tr>
+<td>
+<input type="password" name="password" placeholder="Password"/>
+</td>
+</tr>
+
+
+
+
+</table
+</body>
+</html>
+
+
+
