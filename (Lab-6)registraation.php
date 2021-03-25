ddf3363a4f39e8a65364e108263af2bd2c8fe16f
purplecse111 <77968398+purplecse111@users.noreply.github.com>
2021-03-26 04:43:09 +0600

    Add files via upload

---
+++ (Lab-6)registraation.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login System</title>
+</head>
+<body>
+<h1>Registration Home Page</h1>
+<table>
+<tr>
+<td>
+<input type="text" name="user_name" placeholder="User Name"/>
+</td>
+</tr>
+
+<tr>
+<td>
+<input type="email" name="email" placeholder="Email"/>
+</td>
+</tr>
+<tr>
+<td>
+<input type="password" name="password" placeholder="Password"/>
+</td>
+</tr>
+
+<tr>
+<td>
+<input type="address" name="address" placeholder="Address"/>
+</td>
+</tr>
+<tr>
+<td>
+<input type="phone_no" name="Phone_no" placeholder="Phone No"/>
+</td>
+</tr>
+
+
+
+</table
+</body>
+</html>
